27898d80196e25dbe63969f75098afdcb83958de
caoj1255 <caoj1255@CA-SU-F106-36.sduhsd.lan>
2018-03-09 15:00:41 -0800

    3/9 unit 11

---
+++ APCSA/src/unit11/GradeTester.java
@@ -0,0 +1,14 @@
+package unit11;
+import java.util.Arrays;
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.util.Arrays.*;
+
+public class GradeTester {
+	public static void main(String[] args){
+		Grade test = new Grade(92.5);
+		System.out.println(test);
+	}
+	
+
+}
